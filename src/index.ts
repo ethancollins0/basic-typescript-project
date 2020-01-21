e6e70b4c01f15145df5ba96f089c4b86b662f61a
Ethan Collins <ethantcollins98@gmail.com>
2020-01-20 21:59:14 -0700

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+// import { User } from "./User";
+// import { Company } from "./Company";
+
+const element = document.getElementById("map");
+const map = new google.maps.Map(element, {
+  zoom: 7,
+  center: {
+    lat: 0,
+    lng: 0
+  }
+});
